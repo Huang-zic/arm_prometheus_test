a97f27231910ca7dc10c0ddd1909a8ba5d65a509
Huang <56730991+Huang-zic@users.noreply.github.com>
2024-07-11 19:49:57 +0800

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -7,13 +7,17 @@ ROOT_DIR=$PWD
 OUTPUT_DIR_Perf=$PWD/perf
 OUTPUT_DIR_Test=$PWD/test
 #删除已有输出结果
-rm -rf perf
-rm -rf test
+#rm -rf perf
+#rm -rf test
 # 创建输出目录（如果不存在）
 mkdir -p "$OUTPUT_DIR_Perf"
 mkdir -p "$OUTPUT_DIR_Test"
 COUNT=0
 count_file=$(mktemp)  # 创建临时文件来存储计数器
+
+TOTAL=$(find "$ROOT_DIR" -name '*_test.go' -exec grep -oP 'func \K(Test\w*)' {} + | wc -l)
+echo "总测试项数量为: $TOTAL"
+
 # 查找所有 _test.go 文件
 find  -name '*_test.go' | while read -r test_file; do
     echo "Processing file: $test_file"
@@ -25,19 +29,27 @@ find  -name '*_test.go' | while read -r test_file; do
     grep -oP 'func \K(Test\w*)' "./$(echo "$test_file" | sed 's:.*/::')" | while read -r test_func; do
         echo $test_func
         # 生成执行命令
-        cmd="go test -v -run ^$test_func$ "
+        cmd="go test -v -run ^$test_func$ -timeout 60m"
         path=$(echo "$test_file" | sed -n 's|.*\./\(.*\)\..*|\1|p' | sed 's|/|#|g')
         file="$test_func-$path"
-        $cmd > $file.txt 2>&1
-        echo $test_func >>$file.txt
-        echo "($test_file)" >>$file.txt
-        mv $file.txt $OUTPUT_DIR_Test
+        if [ -f "$OUTPUT_DIR_Test/$file.txt" ]; then
+            echo "the test has completed"
+        else
+            $cmd > $file.txt 2>&1
+            echo $test_func >>$file.txt
+            echo "($test_file)" >>$file.txt
+            mv $file.txt $OUTPUT_DIR_Test
+        fi
         # 调用 perf 脚本
-        $ROOT_DIR/performance_counter_920.sh "$cmd" "$OUTPUT_DIR_Perf" "$test_file"
+        if [ -f "$OUTPUT_DIR_Perf/$file.txt" ]; then
+            echo "the perf test has completed"
+        else
+            $ROOT_DIR/performance_counter_920.sh "$cmd" "$OUTPUT_DIR_Perf" "$test_file"
+        fi
         # 更新计数器
         COUNT=$((COUNT + 1))
         echo $COUNT > "$count_file"
-        echo "Number of tests completed:$COUNT"
+        echo "rate of progress: $COUNT/$TOTAL "
         echo ""
     done
     cd $ROOT_DIR
